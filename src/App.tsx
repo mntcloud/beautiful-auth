a2d2c182d54fc23305eb7f21f8d753395c84fa27
mntcloud <40216914+mntcloud@users.noreply.github.com>
2024-11-24 10:04:04 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,87 @@
+import { useState } from 'react';
+import { fetch } from './mock';
+import './App.css';
+
+function App() {
+  const [email, setEmail] = useState<string | undefined>();
+  const [password, setPassword] = useState<string | undefined>();
+
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [error, setError] = useState<string | undefined>();
+
+  const onSubmit: React.MouseEventHandler<HTMLButtonElement> = async (ev) => {
+    ev.preventDefault();
+    
+    const req = new Request(
+      "auth",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password })
+      }
+    );
+
+    const resp = await fetch(req);
+
+    if (resp.status == 200) {
+      setLoggedIn(true);
+      setError(undefined);
+      
+      // NOTE: ставлю пустую строку так как надо обновить атрибут value у input
+      setEmail(""); 
+      setPassword("");
+    } else {
+      setError("Please check your email or password");
+      setEmail("");
+      setPassword("");
+    }
+  }
+
+  if (isLoggedIn) {
+    return (
+      <>
+        <h1>You're logged in</h1>
+        <button onClick={() => setLoggedIn(false)}>Sign out</button>
+      </>
+    );
+  } else {
+    return (
+      <>
+        {error ?
+          <div className='error'>
+            <div className='tag'>Error!</div>
+            <span>{error}</span>
+          </div> : <></>
+        }
+
+        <form>
+          <label htmlFor='email'>Email</label>
+          <input 
+            type="text" 
+            placeholder='moss@reynholm.co' 
+            value={email} 
+            id="email" 
+            onChange={(ev) => setEmail(ev.currentTarget.value)} />
+          
+          <label htmlFor='pass'>Password</label>
+          <input 
+            type="password" 
+            placeholder='0118 9999'
+            value={password} 
+            id='pass' 
+            onChange={(ev) => setPassword(ev.currentTarget.value)} />
+          
+          <button 
+            type='submit' 
+            onClick={email && password ? onSubmit : undefined} 
+            className={email && password ? 'enabled' : 'disabled'}
+          >Login</button>
+        </form>
+      </>
+    );
+  }
+}
+
+export default App
---
+++ src/mock.ts
@@ -0,0 +1,37 @@
+
+export async function fetch(resource: string | Request, options?: RequestInit) {
+  if (resource instanceof Request) {
+    if (resource.url != "http://localhost:5173/auth") {
+      return new Response(null, {
+        status: 404,
+        statusText: "not found"
+      });
+    }
+
+    if (resource.method != "POST" && resource.headers.get("Content-Type") != "application/json") {
+      return new Response(null, {
+        status: 403,
+        statusText: "access forbidden"
+      });
+    }
+
+    const payload = await resource.json();
+
+    if (payload["username"] != "moss@reynholm.co" && payload["password"] != "01189999") {
+      return new Response(null, {
+        status: 403,
+        statusText: "access forbidden"
+      })
+    }
+
+    return new Response(null, {
+      status: 200,
+      statusText: "Access Granted",
+    });
+  } else {
+    return new Response(null, {
+      status: 404,
+      statusText: "not found",
+    })
+  }
+}
