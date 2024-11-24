bce6bdd085eb3062be43e93aaec57a446fd0a518
mntcloud <40216914+mntcloud@users.noreply.github.com>
2024-11-24 10:50:46 +0300

    fix: avoid undefined value attribute warning with setting state to empty string

--- src/App.tsx
+++ src/App.tsx
@@ -3,39 +3,41 @@ import { fetch } from './mock';
 import './App.css';
 
 function App() {
-  const [email, setEmail] = useState<string | undefined>();
-  const [password, setPassword] = useState<string | undefined>();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const [isLoggedIn, setLoggedIn] = useState(false);
   const [error, setError] = useState<string | undefined>();
 
   const onSubmit: React.MouseEventHandler<HTMLButtonElement> = async (ev) => {
     ev.preventDefault();
-    
-    const req = new Request(
-      "auth",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email, password })
+
+    if (email && password) {
+      const req = new Request(
+        "auth",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, password })
+        }
+      );
+
+      const resp = await fetch(req);
+
+      if (resp.status == 200) {
+        setLoggedIn(true);
+        setError(undefined);
+
+        // NOTE: ставлю пустую строку так как надо обновить атрибут value у input
+        setEmail("");
+        setPassword("");
+      } else {
+        setError("Please check your email or password");
+        setEmail("");
+        setPassword("");
       }
-    );
-
-    const resp = await fetch(req);
-
-    if (resp.status == 200) {
-      setLoggedIn(true);
-      setError(undefined);
-      
-      // NOTE: ставлю пустую строку так как надо обновить атрибут value у input
-      setEmail(""); 
-      setPassword("");
-    } else {
-      setError("Please check your email or password");
-      setEmail("");
-      setPassword("");
     }
   }
 
@@ -58,24 +60,24 @@ function App() {
 
         <form>
           <label htmlFor='email'>Email</label>
-          <input 
-            type="text" 
-            placeholder='moss@reynholm.co' 
-            value={email} 
-            id="email" 
+          <input
+            type="text"
+            placeholder='moss@reynholm.co'
+            value={email}
+            id="email"
             onChange={(ev) => setEmail(ev.currentTarget.value)} />
-          
+
           <label htmlFor='pass'>Password</label>
-          <input 
-            type="password" 
+          <input
+            type="password"
             placeholder='0118 9999'
-            value={password} 
-            id='pass' 
+            value={password}
+            id='pass'
             onChange={(ev) => setPassword(ev.currentTarget.value)} />
-          
-          <button 
-            type='submit' 
-            onClick={email && password ? onSubmit : undefined} 
+
+          <button
+            type='submit'
+            onClick={onSubmit}
             className={email && password ? 'enabled' : 'disabled'}
           >Login</button>
         </form>
